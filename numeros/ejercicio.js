abe4e20e026c792f45785ce5f436ad194320e49a
Adrian Coronel Mendoza <adrianonel78@gmail.com>
2023-01-15 16:41:06 -0500

    Insertación de comentarios de tareas a cumplir en el ejercicio

--- numeros/ejercicio.js
+++ numeros/ejercicio.js
@@ -3,8 +3,11 @@ let alturaMt = 1.70;
 let pesoKg = 110.5;
 let alturaMtRound = alturaMt.toFixed(0);
 let pesoKgRound = pesoKg.toFixed(2);
+
+// Redondear la altura hacia arriba y el peso hacia abajo
 console.log(alturaMtRound); // 2
 console.log(pesoKgRound); // 110.50
 
+// Igualar el maximo valor de JS con el maximo valor de JS + 1 
 let areEqualsMaximuns = (Number.MAX_VALUE + 1) == Number.MAX_VALUE;
 console.log( areEqualsMaximuns ); // true
